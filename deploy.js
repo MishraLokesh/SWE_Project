991cfe03dfcc141dc14911ff7b01f266081b6485
Lokesh Mishra <4man.mishra@gmail.com>
2021-05-09 00:59:24 +0530

    .env resolved the erro and added to .gitignore

--- deploy.js
+++ deploy.js
@@ -3,8 +3,11 @@ const HDWalletProvider = require('@truffle/hdwallet-provider')
 const Web3 = require('web3')
 const {abi, bytecode} = require('./compile.js');
 
+// console.log(  process.env.mnemonic, `https://rinkeby.infura.io/v3/${process.env.infura_API}` )
+
+
 let provider = new HDWalletProvider(
-  process.env.mnemonic, process.env.infura_API
+  process.env.mnemonic, `https://rinkeby.infura.io/v3/${process.env.infura_API}`
 );
 
 const web3 = new Web3(provider)
